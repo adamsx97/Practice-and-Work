662478ba825157c8533c286dc37dd427d90ae5f7
scherukuri42 <scherukuri42@gmail.com>
2016-03-31 20:42:47 -0700

    2016-03-31 8:42 Updated LongInt.h

--- 201603ECS60FILES/p1/xcgp1/LongInt.h
+++ 201603ECS60FILES/p1/xcgp1/LongInt.h
@@ -1,16 +1,31 @@
 // 2016-03-31 19:58
 // Author Chengeng Xiao and Srikanth Cherukuri 
 // ECS60 HW1
+#include <iostream>
+#ifndef	 LONGINT_H
+#define  LONGINT_H
 
 class ListNode
 {
+public:
 	ListNode *prev, *next;
 	int num;
+	ListNode() : prev(NULL), next(NULL) {}
+	friend class LongInt;
 }; // class ListNode
 
 class LongInt
 {
-	LN* head, tail, curr;
-	insert
+	ListNode *head, *tail, *curr;
+public:
+	LongInt() : head(NULL), tail(NULL), curr(NULL) {}
+	void insert(int n);
+	bool isempty();
+	int pop_back();
+	std::ostream& operator<<(std::ostream& os, LongInt& obj);
+	std::istream& operator>>(std::istream& is, LongInt& obj);
+	void operator=(const LongInt& object) const;
+	void operator+(const LongInt& object) const;
 
 }; // class LongInt a linked list
+#endif
