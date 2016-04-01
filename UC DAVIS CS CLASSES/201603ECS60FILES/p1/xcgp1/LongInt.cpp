610ab8d67268ad7c653427e5148b864262a63c5a
CG Xiao <adamsxiao6197@gmail.com>
2016-03-31 20:37:48 -0700

    2016-03-31 20:37 Update LongInt.cpp

--- UC DAVIS CS CLASSES/201603ECS60FILES/p1/xcgp1/LongInt.cpp
+++ UC DAVIS CS CLASSES/201603ECS60FILES/p1/xcgp1/LongInt.cpp
@@ -3,5 +3,51 @@
 // ECS60 HW1
 
 #include <iostream>
+#include <string>
 #include "LongInt.h"
 
+using namespace std;
+
+typedef string::iterator SIT;
+
+LongInt::LongInt()
+{
+  head = NULL;
+  tail = NULL;
+  curr = NULL;
+} //constructor
+
+void LongInt::insert(ListNode *newLN)
+{
+  tail->next = newLN;
+  newLN->prev = tail;
+  tail = newLN;
+} // insert()
+
+bool LongInt::isempty()
+{
+  if(head)
+    return 1;
+  return 0;
+} // isempty()
+
+int LongInt::pop_back()
+{
+  int buf;
+  if(tail != head)
+    tail = tail->prev;
+  else
+    head = NULL;
+  return buf;
+} // pop_back()
+
+int main() {
+  LongInt int1, int2, int3;
+  cout << "Please enter first long integer >> ";
+  cin >> int1;
+  cout << "Please enter second long integer >> ";
+  cin >> int2;
+  int3 = int1 + int2;
+  cout << int3 << endl;
+  return 0;
+} // main()
