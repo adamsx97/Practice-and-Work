6d858ad645348c7d22fc236729d78cb7527b4663
CG Xiao <adamsxiao6197@gmail.com>
2016-04-06 20:47:15 -0700

    2016-04-06 20:46 Update ECS60 Longint.h.

--- 201603ECS60FILES/p1/xcgp1/LongInt.h
+++ 201603ECS60FILES/p1/xcgp1/LongInt.h
@@ -1,32 +1,24 @@
 // 2016-03-31 19:58
 // Author Chengeng Xiao and Srikanth Cherukuri 
 // ECS60 HW1
-#include <iostream>
-#ifndef	 LONGINT_H
-#define  LONGINT_H
 
-class ListNode
-{
-public:
-	ListNode *prev, *next;
-	int num;
-	ListNode(int n) : prev(NULL), next(NULL), num(n) {}
-	friend class LongInt;
-}; // class ListNode
+#ifndef  LONGINT_H
+#define  LONGINT_H
+#include "StackLi.h"
+
+using namespace std;
 
 class LongInt
 {
-	ListNode *head, *tail, *curr;
+private:
+  StackLi<int> data;
 public:
-	LongInt() : head(NULL), tail(NULL), curr(NULL) {}
-	void insertT(ListNode *newLN);
-	void insertH(ListNode *newLN);
-	bool isempty();
-	int pop_back();
-	friend std::ostream& operator<<(std::ostream& os, LongInt& obj);
-	friend std::istream& operator>>(std::istream& is, LongInt& obj);
-	void operator=(const LongInt& object);
-	LongInt operator+(const LongInt& object);
+  LongInt();
+  void insert(ListNode *newLN);
+  friend ostream& operator<<(ostream& os, LongInt& obj);
+  friend istream& operator>>(istream& is, LongInt& obj);
+  void operator=(const LongInt& object);
+  LongInt operator+(const LongInt& object);
 
 }; // class LongInt a linked list
 #endif
