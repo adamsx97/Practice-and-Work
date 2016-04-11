eca8f798e1aa8ea55b5086c619f9efc29f30e96d
cgxiao97 <adamsxiao6197@gmail.com>
2016-04-11 13:49:43 -0700

    2016-04-11 13:49 Edit ECS50 hw1 Add ECS198 hw1

--- 201603ECS50FILES/Homework1/scientificFloating/scientificFloating.cpp
+++ 201603ECS50FILES/Homework1/scientificFloating/scientificFloating.cpp
@@ -9,7 +9,7 @@ using namespace std;
 typedef union{
 	float f;
 	unsigned int fn;
-} FFF; // union FFF
+} FFF; // union FFF Float_For_Format
 
 int main(void)
 {
@@ -32,51 +32,27 @@ int main(void)
 
     if(n & 0x80000000)
         cout << '-';
-    unsigned bit = n & 0x07FFFFF;
     cout << "1.";
     // sign and before decimal point
 
+    unsigned buf = n;
     for(int i = 0; i< 23 ; ++i){
-        bit <<=1;
-        if (bit & 0x0800000)
+        buf <<=1;
+        if (buf & 0x0800000)
             bitwise.append("1");
         else // else
             bitwise.append("0");
-    } // for()
+    } // for(): calc for 23 bits
+
     for (string::iterator itr = bitwise.end() - 1; *itr == '0'; itr--)
         bitwise.pop_back();
     cout << bitwise;
     // bitwise number
 
     cout << "E";
-    int exp = (n >> 23) & 0x0FF;
-
+    int exp = (n >> 23) & 0xff;
     exp -= 127; //bias 127
-    if(exp < 0)
-    {
-        cout << '-';
-        exp = -exp;
-    } // if()
-    // exponent sign
-
-    int i = 0;
-
-    while((exp & 0x080) == 0 && i < 8)
-    {
-        exp <<= 1;
-        ++i;
-    } // skip zero of top
-
-    int expo = 0;
-
-    if(i == 8)
-   		cout << '0';
-    for(;i< 8 ; ++i){
-        if(exp & 0x080)
-            expo += pow(2, 7 - i);
-        exp <<=1;
-    }
-    cout << expo << endl;
+    cout << exp << endl;
     // exponent number
 
 	return 0;
