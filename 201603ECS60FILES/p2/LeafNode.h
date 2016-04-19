6b741ce56d222738c19fb6cb4e1e398c1088774a
cgxiao97 <adamsxiao6197@gmail.com>
2016-04-19 00:53:57 -0700

    2016-04-19 12:53 Add ECS60 p2

---
+++ 201603ECS60FILES/p2/LeafNode.h
@@ -0,0 +1,29 @@
+#ifndef LeafNodeH
+#define LeafNodeH
+
+#include "BTreeNode.h"
+
+/*
+  int count;
+  int leafSize;
+  InternalNode *parent;
+  BTreeNode *leftSibling;
+  BTreeNode *rightSibling;
+*/
+
+class LeafNode:public BTreeNode
+{
+  int *values;
+public:
+  LeafNode(int LSize, InternalNode *p, BTreeNode *left,
+    BTreeNode *right);
+  int getMinimum() const;
+  LeafNode* insert(int value); // returns pointer to new Leaf if splits
+  // else NULL
+  void print(Queue <BTreeNode*> &queue);
+  LeafNode* split();
+  void insertDirectly(int value);
+  void moveToLeft();
+}; //LeafNode class
+
+#endif
