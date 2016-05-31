1ec53dee44067bf9ddcdcb1ec278e8a4aed8ec3c
cgxiao97 <adamsxiao6197@gmail.com>
2016-05-30 23:42:13 -0700

    2016-05-30 23:42 Update ECS50HW4

--- UC DAVIS CS CLASSES/201603ECS50FILES/Homework4/MyFloat.cpp
+++ UC DAVIS CS CLASSES/201603ECS50FILES/Homework4/MyFloat.cpp
@@ -33,14 +33,83 @@ bool MyFloat::operator==(const float rhs) const{
 
 MyFloat MyFloat::operator+(const MyFloat& rhs) const{
 	MyFloat small, big;
-	if (this->exponent == 0)
+  int expDiff = 0;
+	
+  if (this->exponent == 0)
     return rhs;
   if (rhs.exponent == 0)
     return *this;
   if (this->sign != rhs.sign && this->mantissa == rhs.mantissa && this->exponent == rhs.exponent)
     return MyFloat(0);
 
-	return *this;
+  expDiff = this->exponent - rhs.exponent;
+  if (this->exponent < rhs.exponent)
+    {
+        small = MyFloat(*this);
+        big = MyFloat(rhs);
+    }
+  else
+    {
+        small = MyFloat(rhs);
+        big = MyFloat(*this);
+    }
+
+  small.mantissa |= (1 << 23);
+  big.mantissa |= (1 << 23);
+
+  if (expDiff)
+  {
+    if  (((small.mantissa >> (expDiff - 1)) & 1) && (big.sign != small.sign))
+    {
+      small.mantissa >>= expDiff;
+      small.mantissa += 1;
+    }
+
+  else
+    small.mantissa >>= expDiff;  
+  }
+
+  small.exponent = big.exponent;
+
+  if (small.sign == big.sign)
+  {
+    big.mantissa += small.mantissa;
+    if (big.mantissa & (1 << 24))
+    {
+      big.mantissa >>= 1;
+      big.exponent++;
+    }
+    big.mantissa &= (1 << 23) - 1;
+  }
+  else
+  {
+    if (big.mantissa < small.mantissa)
+    {
+      big.mantissa = small.mantissa - big.mantissa;
+      big.sign = small.sign;
+    }
+    else
+      big.mantissa = big.mantissa - small.mantissa;
+
+    if (big.mantissa == 0)
+    {
+      big.sign = 0
+      big.exponent = 0;
+    }
+    else
+    {
+      int i;
+      while(!(big.mantissa & (1 << 23)))
+      {
+        i++;
+        big.mantissa <<= 1;
+      }
+      big.mantissa &= (1 << 23) - 1;
+      big.exponent -= i;
+    }
+  }
+
+	return big;
 } // overloaded operator +
 
 MyFloat MyFloat::operator-(const MyFloat& rhs) const{
@@ -52,22 +121,24 @@ MyFloat MyFloat::operator-(const MyFloat& rhs) const{
 
 void MyFloat::unpackFloat(float f) {
 
-	__asm__(
-		//this->sign = (i >> 31); 
-		"shr $31, %%eax;"
+  __asm__(
+    //this->sign = (i >> 31); 
+    "shr $31, %%eax;"
 
-		//this->exponent = (i << 1) >> 23;  
-		"shl $1, %%ebx;"
-		"shr $24, %%ebx;"
+    //this->exponent = (i >> 23) & 0xFF;  
+    "shr $23, %%ebx;"
+    "and $0xFF, %%ebx;"
 
-		//this->mantissa = (i << 9) >> 9;
-		"shl $9, %%ecx;"
-		"shr $9, %%ecx":
+    //this->mantissa = i & ((1 << 23) -1);
+    "movl $1, %%edi;"
+    "shl $23, %%edi;"
+    "subl $1, %%edi;"
+    "and %%edi, %%ecx":
 
-		"=a" (sign), "=b" (exponent), "=c" (mantissa):
-		"a" (f), "b" (f), "c" (f):
-		"cc"
-	);
+    "=a" (sign), "=b" (exponent), "=c" (mantissa): // output
+    "a" (f), "b" (f), "c" (f): // input
+    "cc"
+  );
 
 }//unpackFloat
 
